0716b387d9fd7ba6f5cac47f0c42a8df8ad8b90d
SABARICHAMP <34640992+SABARICHAMP@users.noreply.github.com>
2017-12-19 04:50:11 +0000

    Add files via upload

---
+++ BEGINNER/ASSIGNMENTS/OPERATORS/operator1.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main(){
+int i = -3;
+int k = i % 2;
+printf("%d", k);
+}
---
+++ BEGINNER/ASSIGNMENTS/OPERATORS/operator_calculator.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(void)
+{
+int a,b;
+printf("enter inputs");
+scanf("%d%d",&a,&b);
+printf("\naddition is %d",a+b);
+printf("\naddition is %d",a-b);
+printf("\naddition is %d",a*b);
+printf("\naddition is %d",a/b);
+printf("\naddition is %d",a%b);
+}
