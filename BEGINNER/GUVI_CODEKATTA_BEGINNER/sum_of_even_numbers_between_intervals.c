e064658640ed3e8037fee6bd3cc96f076328e30d
SABARICHAMP <34640992+SABARICHAMP@users.noreply.github.com>
2018-03-08 10:10:22 +0530

    Create sum_of_even_numbers_between_intervals.c

---
+++ BEGINNER/GUVI_CODEKATTA_BEGINNER/sum_of_even_numbers_between_intervals.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+int main(void)
+{
+  int x,y,i;
+  scanf("%d%d",&a,&b);
+  for(i=x+1;i<y;i++)
+  {
+    if(i%2==0)
+    {
+       printf("%d\n",i);
+     }
+   }
+   return 0;
+}
