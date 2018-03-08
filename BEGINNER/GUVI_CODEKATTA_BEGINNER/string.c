a4068964cb07e6fd92b943dc8cdb6e74ab5edc3a
SABARICHAMP <34640992+SABARICHAMP@users.noreply.github.com>
2018-03-09 01:47:04 +0530

    Create string.c

---
+++ BEGINNER/GUVI_CODEKATTA_BEGINNER/string.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+char str[5];
+  int k,i;
+printf("enter a string");
+scanf("%s",&str);
+printf("enter a limit");
+scanf("%d",&k);
+for(i=0;i<=k;i++)
+{
+printf("%s",str);
+}
+}
