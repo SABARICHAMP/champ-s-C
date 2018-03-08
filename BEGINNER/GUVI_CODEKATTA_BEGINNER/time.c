d77d4bc7192916ef88600743427457c102684cfc
SABARICHAMP <34640992+SABARICHAMP@users.noreply.github.com>
2018-03-09 01:18:49 +0530

    Update time.c

--- BEGINNER/GUVI_CODEKATTA_BEGINNER/time.c
+++ BEGINNER/GUVI_CODEKATTA_BEGINNER/time.c
@@ -2,9 +2,9 @@
 void main()
 {
 int min,hr;
-printf("enter the minute");
+//printf("enter the minute");
 scanf("%d",&min);
 hr=min/60;
 min=min%60;
-printf("%d\t%d",hr,min);
+//printf("%d\t%d",hr,min);
 }
