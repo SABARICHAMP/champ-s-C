a7b2c972ed38458ff53e9cbb43ee4d67b30ec478
SABARICHAMP <34640992+SABARICHAMP@users.noreply.github.com>
2018-03-08 09:33:24 +0530

    Update vowel_or_consonant.c

--- BEGINNER/GUVI_CODEKATTA_BEGINNER/vowel_or_consonant.c
+++ BEGINNER/GUVI_CODEKATTA_BEGINNER/vowel_or_consonant.c
@@ -1,14 +1,16 @@
 #include<stdio.h>
 int main()
 {
-char c;
-scanf("%c",&c);
-if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u'||c=='A'||c=='E'||c=='I'||c=='O'||c=='U')
+char a;
+scanf("%c",&a);
+if(a=='a'||a=='e'||a=='i'||a=='o'||a=='u'||a=='A'||a=='E'||a=='I'||a=='O'||a=='U'||)
 {
-printf("\nvowel");
+  printf("\n");
+   printf("vowel");
 }
 else
 {
-printf("\nconsonant");
+  printf("\n");
+  printf("consonant");
 }
 }
