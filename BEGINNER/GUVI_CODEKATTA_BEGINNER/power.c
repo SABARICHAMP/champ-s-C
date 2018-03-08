1c45771e6b017ef1e2e4216f74c69db2a4abf5f8
SABARICHAMP <34640992+SABARICHAMP@users.noreply.github.com>
2018-03-09 02:06:12 +0530

    Update power.c

--- BEGINNER/GUVI_CODEKATTA_BEGINNER/power.c
+++ BEGINNER/GUVI_CODEKATTA_BEGINNER/power.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 int main()
 {
-	int n,i,x=1,pow=2;
+	int n,i,x=1,power=2;
 	scanf("%d",&n);
 	for(i=1;i<=n;i++)
 	{
-	    x=x*pow;
+	    x=x*power;
 	    if(x==n)
 	    {
 	        printf("%d is a power of 2",n);
