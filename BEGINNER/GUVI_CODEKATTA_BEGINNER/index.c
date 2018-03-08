ceaf59653ed26cb1c299eb07e98fee764e90d2f2
SABARICHAMP <34640992+SABARICHAMP@users.noreply.github.com>
2018-03-09 01:16:23 +0530

    Update index.c

--- BEGINNER/GUVI_CODEKATTA_BEGINNER/index.c
+++ BEGINNER/GUVI_CODEKATTA_BEGINNER/index.c
@@ -4,14 +4,14 @@ void main()
 int a[10],i,n;
 printf("enter the limimt");
 scanf("%d",&n);
-printf("enter the element");
+//printf("enter the element");
 for(i=0;i<n;i++)
 {
 scanf("%d",&a[i]);
 }
-printf("the elements with index");
+//printf("the elements with index");
 for(i=0;i<n;i++)
 {
-printf("%d\t%d\n",i,a[i]);
+printf("%d %d",i,a[i]);
 }
 }
