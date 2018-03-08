20cdb1ec439c876f673a783c2a25c5b81296b98f
SABARICHAMP <34640992+SABARICHAMP@users.noreply.github.com>
2018-03-09 02:19:54 +0530

    Update 14.py

--- BEGINNER/GUVI_CODEKATTA_BEGINNER/14.py
+++ BEGINNER/GUVI_CODEKATTA_BEGINNER/14.py
@@ -1,11 +1,11 @@
 ip=input("Enter your K1 and K2 value in single line with space:")
-k1=int(ip.split(" ")[0].strip())
-k2=int(ip.split(" ")[1].strip())
+k11=int(ip.split(" ")[0].strip())
+k22=int(ip.split(" ")[1].strip())
 print("Enter your array:")
 st=[]
-for x in range(k1):
+for x in range(k11):
 	a=int(input())
 	st.append(a)
-for x in range(k2-1):
+for x in range(k22-1):
 	st.remove(min(st))
 print("Ans is:",min(st))
