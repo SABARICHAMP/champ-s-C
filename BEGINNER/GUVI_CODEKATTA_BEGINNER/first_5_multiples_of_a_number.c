65d9a730d5df7d662754e319f177f704426c60eb
SABARICHAMP <34640992+SABARICHAMP@users.noreply.github.com>
2018-03-08 10:43:01 +0530

    Create first_5_multiples_of_a_number.c

---
+++ BEGINNER/GUVI_CODEKATTA_BEGINNER/first_5_multiples_of_a_number.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+    int main()
+    {
+        int i,x,m;
+        scanf("%d",&n);
+        for(i=1;i<=x;i++)
+        {
+            m=x*i;
+        
+        printf("%d  ",m);
+        }
+        return 0;
+    }
