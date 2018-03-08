e4a75a87a01f4d1779329c163a5d5c75df09c71e
SABARICHAMP <34640992+SABARICHAMP@users.noreply.github.com>
2018-03-08 10:06:24 +0530

    Create odd_numbers_between_intervals.c

---
+++ BEGINNER/GUVI_CODEKATTA_BEGINNER/odd_numbers_between_intervals.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+int main(void)
+{
+    int x,y;
+    int i;
+    scanf("%d%d",&x,&y);
+    for(i=a+1;i<b;i++)
+    {
+        if(i%2!=0)
+        {
+            printf("%d",i);
+        }
+    }
+return 0;
+}
