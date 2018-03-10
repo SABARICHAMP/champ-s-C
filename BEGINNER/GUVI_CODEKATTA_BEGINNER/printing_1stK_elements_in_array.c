7b5a9ef759415b39c5baaaba4c9e94315d7fc1a3
SABARICHAMP <34640992+SABARICHAMP@users.noreply.github.com>
2018-03-10 14:22:10 +0530

    Create printing_1stK_elements_in_array.c

---
+++ BEGINNER/GUVI_CODEKATTA_BEGINNER/printing_1stK_elements_in_array.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(void)
+{
+    char a[100],k;
+     scanf("%s",a);
+     scanf("%d",&k);
+     for(int i=0;i<k;i++)
+     {
+         printf("%c",a[i]);
+     }
+    
+    
+}
