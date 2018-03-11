de63377ef0d562e88addc290aad7ef2079861790
SABARICHAMP <34640992+SABARICHAMP@users.noreply.github.com>
2018-03-11 21:19:26 +0530

    Create nearest_greater_power_of_2.c

---
+++ BEGINNER/GUVI_CODEKATTA_BEGINNER/nearest_greater_power_of_2.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(void)
+{
+	int no,i,x,y,d;
+	printf("enter the number:");
+	scanf("%d",&no);
+	for(i=1;i<=no;i++)
+	{
+		aa=pow(2,i);
+		if(x>n)
+		{
+			d=i;
+			break;
+		}
+	}
+	y=pow(2,d);
+	printf("\n%d",y);
+	
+	return 0;
+}
