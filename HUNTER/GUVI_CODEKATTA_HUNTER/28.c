c20e15de9c99c02948f379457d7379134f34aeea
SABARICHAMP <34640992+SABARICHAMP@users.noreply.github.com>
2018-06-04 16:49:45 +0530

    Create 28.c

---
+++ HUNTER/GUVI_CODEKATTA_HUNTER/28.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+  	char s[100];
+  	int i, j, k;
+  	gets(s);
+  	 	
+  	for(i = 0; i < strlen(s); i++)
+  	{
+  		for(j = i + 1; s[j] != '\0'; j++)
+  		{
+  			if(s[j] == s[i])  
+			{
+  				for(k = j; s[k] != '\0'; k++)
+				{
+					s[k] = s[k + 1];
+				}
+ 			}
+		}
+	}
+	printf("\n The Final String after Removing All Duplicates = %s ", s);
+  	return 0;
+}
