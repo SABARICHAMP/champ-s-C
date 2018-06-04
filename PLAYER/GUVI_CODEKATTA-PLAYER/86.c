cfb22c3c668c2928fcfa436c62e2d51afe152bb2
SABARICHAMP <34640992+SABARICHAMP@users.noreply.github.com>
2018-06-04 14:29:22 +0530

    Create 86.c

---
+++ PLAYER/GUVI_CODEKATTA-PLAYER/86.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+
+int main(void){
+	int num,temp=0;
+	scanf("%d",&num);
+	int a[num];
+	int i;
+	for(i=0;i<num;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	for(i=0;i<num;i++)
+	{
+		temp=temp^a[i];
+	}
+	printf("%d",temp);
+	
+
+	return 0;
+}
