fead8440f368d984535c56d12a91a5af1cc754b6
SABARICHAMP <34640992+SABARICHAMP@users.noreply.github.com>
2018-06-04 15:19:41 +0530

    Create 99.c

---
+++ PLAYER/GUVI_CODEKATTA-PLAYER/99.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <math.h>
+int main(void) {
+	long int num,rem=0,i=1,sum=0;
+
+	scanf("%ld",&num);
+	while(num!=0)
+	{
+		rem=num%10;
+		sum=sum+rem*i;
+		num=num/10;
+		i=i*2;
+	}
+	printf("%lo",sum);
+	return 0;
+}
