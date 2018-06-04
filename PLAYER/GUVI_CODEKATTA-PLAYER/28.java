327058e9ac99a1ce441ba31fdce86f11ce72326e
SABARICHAMP <34640992+SABARICHAMP@users.noreply.github.com>
2018-06-04 12:42:13 +0530

    Create 28.java

---
+++ PLAYER/GUVI_CODEKATTA-PLAYER/28.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		String str=s.nextLine();
+		String str1=str.replaceAll(" ","");
+		System.out.println(str1);
+	}
+}
