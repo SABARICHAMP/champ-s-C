b8e3b5c27ebc2052a68935e5e71acfceb1b9104d
SABARICHAMP <34640992+SABARICHAMP@users.noreply.github.com>
2018-06-04 11:35:08 +0530

    Create 3.c

---
+++ PLAYER/GUVI_CODEKATTA-PLAYER/3.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(void) 
+{ 
+int num,reverse,r=0;
+scanf("%d",&num);
+while(num!=0)
+{
+r=num%10;
+reverse=reverse*10+r;
+num=num/10;
+}
+printf("%d",reverse);
+return 0;
+}
