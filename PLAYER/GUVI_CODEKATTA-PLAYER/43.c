9e32f4b163312b9970c0b934fda57e63ac9be8f3
SABARICHAMP <34640992+SABARICHAMP@users.noreply.github.com>
2018-06-04 13:31:48 +0530

    Create 43.c

---
+++ PLAYER/GUVI_CODEKATTA-PLAYER/43.c
@@ -0,0 +1,15 @@
+import java.util.*;
+public class subb {
+  public static void main(String aa[]){
+      int n;
+      Scanner s=new Scanner(System.in);
+      String s1=s.next();
+      String s2=s.next();
+      if(s1.contains(s2)){
+          System.out.println("yes");
+      }
+ else{
+          System.out.println("No");
+ }
+      }
+    }
