56d4f6d9432a8e79c482960c21f4232f637e8a35
SABARICHAMP <34640992+SABARICHAMP@users.noreply.github.com>
2018-06-04 15:36:44 +0530

    Update 120.c

--- PLAYER/GUVI_CODEKATTA-PLAYER/120.c
+++ PLAYER/GUVI_CODEKATTA-PLAYER/120.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 int main(void){
-	int num,rem,j,sum=0;
+	int num,remainder,j,sum=0;
 	scanf("%d",&num);
 	while(num>0)
 	{
-		rem=num%10;
-		j=rem*rem;
+		remainder=num%10;
+		j=remainder*remainder;
 		sum=sum+j;
 		num=num/10;
 	}
